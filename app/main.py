0dca5bbbb96a1af8366cbf59ead441364e5a892f
DamianDobrzycki1 <118450532+DamianDobrzycki1@users.noreply.github.com>
2026-09-18 10:39:27 +0000

    Commits

--- app/main.py
+++ app/main.py
@@ -5,3 +5,7 @@ app = FastAPI(title= "lab1 - FastAPI User Api")
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+@app.get("/hello")
+def hello():
+    return {"message": "Hello World"}
