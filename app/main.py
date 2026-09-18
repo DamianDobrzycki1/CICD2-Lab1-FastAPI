8edd38528bb8dc405022f83833935626e2774e21
DamianDobrzycki1 <118450532+DamianDobrzycki1@users.noreply.github.com>
2026-09-18 11:10:23 +0000

    Added a Delete user

--- app/main.py
+++ app/main.py
@@ -21,3 +21,15 @@ def add_user(new_user: UserCreate):
                                 detail="A user with this id already exists")
     users.append(new_user)
     return new_user
+
+@app.delete("/api/users/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(user_id: int):
+    for index, existing_user in enumerate(users):
+        if existing_user.user_id == user_id:
+            users.pop(index)
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="User no found",
+    )
