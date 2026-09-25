52eb67c7765e96b445a24bd3057bed342c01bb40
DamianDobrzycki1 <118450532+DamianDobrzycki1@users.noreply.github.com>
2026-09-25 11:06:49 +0000

    Added test for duplicate_user

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,5 +1,5 @@
 def user_payload(uid=1, name="Damian", email="G00419511@atu.ie", age=21, student_id="G00419511"):
-    return {"userid" : uid, "name": name, "email": email, "age": age, "student_id": student_id}
+    return{"userid": uid, "name": name, "email": email, "age": age, "student_id": student_id}
 
 def test_create_user_returns_201(client):
     response = client.post("api/users", json=user_payload())
@@ -16,4 +16,4 @@ def test_duplicate_user_id_returns_409(client):
     response = client.post("/api/users", json=user_payload(uid=2))
 
     assert response.status_code == 409
-    assert "exists" in response.json()["details"].lower()
+    assert "exists" in response.json()["detail"].lower()
