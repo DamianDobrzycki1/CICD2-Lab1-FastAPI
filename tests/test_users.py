a0212bbcce90436a5c48d11689fc53ed37c87bd3
DamianDobrzycki1 <118450532+DamianDobrzycki1@users.noreply.github.com>
2026-09-25 11:12:55 +0000

    Added testing for bad student id

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,3 +1,4 @@
+import pytest
 def user_payload(uid=1, name="Damian", email="G00419511@atu.ie", age=21, student_id="G00419511"):
     return{"userid": uid, "name": name, "email": email, "age": age, "student_id": student_id}
 
@@ -17,3 +18,9 @@ def test_duplicate_user_id_returns_409(client):
 
     assert response.status_code == 409
     assert "exists" in response.json()["detail"].lower()
+
+@pytest.mark.parametrize("bad_student_id", ["12345672", "g0043584", "G002", "G4091252"])
+
+def test_bad_student_id_returns_422(client, bad_student_id):
+    response = client.post("/api/users", json=user_payload(uid=3, student_id=bad_student_id))
+    assert response.status_code == 422
